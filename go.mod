fa700a4a14d2ec9634aee6f3a797500f5a468b2a
mguitar24 <mguitar24@gmail.com>
2025-02-11 22:42:46 -0700

    init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/MGuitar24/drudge
+
+go 1.23.3
+
+require github.com/MGuitar24/godrudge v0.0.0-20250212051705-6282d1cc4734
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.1 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/term v0.27.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/MGuitar24/godrudge"
+)
+
+// Version variable (will be set via -ldflags)
+var Version = "dev" // Default value for local builds
+
+func main() {
+	if len(os.Args) > 1 {
+		flag := os.Args[1]
+		if flag == "-v" {
+			fmt.Println("Version:", Version)
+			os.Exit(0)
+		}
+	}
+
+	client := godrudge.NewClient()
+	err := client.Parse()
+	if err != nil {
+		fmt.Println("Error parsing", err)
+	}
+	client.PrintHeadlines()
+}
