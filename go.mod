1ea3269f55fc11a4f8d29f25144375bc889bd2db
mguitar24 <mguitar24@gmail.com>
2025-02-15 22:25:32 -0700

    watch mode added

--- go.mod
+++ go.mod
@@ -2,12 +2,20 @@ module github.com/MGuitar24/drudge
 
 go 1.24.0
 
-require github.com/MGuitar24/godrudge v1.1.4
+require (
+	github.com/MGuitar24/godrudge v1.1.4
+	github.com/mmcdole/gofeed v1.3.0
+)
 
 require (
 	github.com/PuerkitoBio/goquery v1.10.2 // indirect
 	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mmcdole/goxpp v1.1.1-0.20240225020742-a0c311522b23 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/term v0.29.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/MGuitar24/godrudge"
+	"github.com/mmcdole/gofeed"
 )
 
 // Version variable (will be set via -ldflags)
@@ -23,15 +27,62 @@ func main() {
 		case "-h":
 			printHelp()
 			os.Exit(0)
+		case "-w":
+			if len(os.Args) > 2 {
+				refreshTime, _ := strconv.Atoi(os.Args[2])
+				startWatch(refreshTime)
+			} else {
+				startWatch(30)
+			}
+			select {}
 		}
 	}
+	runAndPrint(stringArg)
+}
 
+func runAndPrint(stringArg bool) {
 	client := godrudge.NewClient()
 	err := client.Parse()
 	if err != nil {
 		fmt.Println("Error parsing", err)
 	}
 	client.PrintDrudge(stringArg)
+}
+
+func startWatch(refreshInterval int) {
+	const rssURL = "http://feeds.feedburner.com/DrudgeReportFeed"
+	var latestPublishedTime *time.Time
+	go func() {
+		for {
+			fp := gofeed.NewParser()
+			feed, err := fp.ParseURL(rssURL)
+			if err != nil {
+				log.Println("Error fetching RSS feed:", err)
+			} else {
+				if len(feed.Items) > 0 {
+					// Only print new items
+					for _, item := range feed.Items {
+						if item.PublishedParsed != nil && (latestPublishedTime == nil || item.PublishedParsed.After(*latestPublishedTime)) {
+							runAndPrint(false)
+							if latestPublishedTime == nil {
+								fmt.Println("Waiting for update from Drudge...")
+							} else {
+								currentTime := time.Now().Format("Mon, 02 Jan 2006 03:04:05 PM MST")
+								fmt.Println(currentTime)
+							}
+							latestPublishedTime = item.PublishedParsed
+							break
+						}
+					}
+				}
+			}
+			if refreshInterval > 30 {
+				time.Sleep(time.Duration(refreshInterval) * time.Second)
+			} else {
+				time.Sleep(30 * time.Second)
+			}
+		}
+	}()
 }
 
 func printHelp() {
